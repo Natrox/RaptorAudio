2d11355014dafb91dd7252297c21bf3d79f0a44d
Natrox <110157@edu.nhtv.nl>
2014-03-14 13:38:12 +0100

    Old main.

--- RaptorAudio/src/main.cpp
+++ RaptorAudio/src/main.cpp
@@ -15,20 +15,11 @@ using namespace Raptor::Utility;
 
 #define RATE 44100
 
-char data[ 10 * 1024 * 1024 ];
-
 int main( void )
 {
 	SoundMixer::InitializeMixer( RATE, 4410, SoundMixerBufferingModes::BUFFERING_BLOCKS, SoundMixerProfiles::SOUND_MIXER_SPEAKERS );
 
-	FILE* file = fopen( "Space Faring.ogg", "rb" );
-
-	int i = 0;
-	while ( fread( data + i, 1, 1, file ) ) i++;
-
-	fclose( file );
-
-	StreamingSoundObject* song = new StreamingSoundObject( data, AudioOrigins::AUDIO_ORIGIN_OPENMEMORY_POINT, i );
+	StreamingSoundObject* song = new StreamingSoundObject( "Space Faring.ogg" );
 	SoundObjectProperties prop = SoundMixer::GetMixer()->CreateProperties( song );
 
 	prop->SetLooping( true );
